1ad8e4c0903ae59bb8b8bf45cf974836adc950c1
Rodolfo Perez Avila <rodolfop@solin.com.co>
2021-09-09 15:52:13 -0500

    Initial commit

---
+++ dna-checker/src/main/java/com/exercise/mutant/webservices/dnachecker/DnaCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.mutant.webservices.dnachecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaCheckerApplication.class, args);
+	}
+
+}
---
+++ dna-checker/src/test/java/com/exercise/mutant/webservices/dnachecker/DnaCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.mutant.webservices.dnachecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
